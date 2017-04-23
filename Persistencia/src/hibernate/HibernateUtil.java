98b4c7f34df869a0d61b67ec6f9b59504633cdc1
Javier Capotondo <jcapotondo@gmail.com>
2017-04-22 22:02:02 -0300

    hibernate funcionando

--- Persistencia/src/hibernate/HibernateUtil.java
+++ Persistencia/src/hibernate/HibernateUtil.java
@@ -3,38 +3,32 @@ package hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.jboss.jandex.Main;
 
 import entity.ClienteEntity;
 
  
-public class HibernateUtil
-{
+public class HibernateUtil{
+	
     private static final SessionFactory sessionFactory;
 
-    static
-    {
-        try
-        {
+    static{
+        try{
+        	
         	 Configuration config = new Configuration();
         	 config.addAnnotatedClass(ClienteEntity.class);
         	 
              sessionFactory = config.buildSessionFactory();
-        }
-        catch (Throwable ex)
-        {
+             
+        }catch (Throwable ex){
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
  
-    public static SessionFactory getSessionFactory()
-    {
+    public static SessionFactory getSessionFactory(){
+    	
         return sessionFactory;
     }
     
-    public static void main(String[] args) {
-    	SessionFactory sf = HibernateUtil.getSessionFactory();
-		Session session = sf.openSession();
-	}
+
 }
