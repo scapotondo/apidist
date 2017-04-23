54f25a999a0b8ee2265cfc1be5ce002021df2fc5
scapotondo <sebastian.capotondo@gmail.com>
2017-04-23 18:20:29 -0300

    Proveedor

--- Persistencia/src/entity/ProveedorEntity.java
+++ Persistencia/src/entity/ProveedorEntity.java
@@ -2,13 +2,14 @@ package entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
+@Embeddable
 @Table(name="Proveedor")
 public class ProveedorEntity implements Serializable{
 	
@@ -16,5 +17,9 @@ public class ProveedorEntity implements Serializable{
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 	
-	
+	private String telefono;
+	private String nombre;
+	private String domicilio;
+	private String cuit;
+	private String cbu;
 }
