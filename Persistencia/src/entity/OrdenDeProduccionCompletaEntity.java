0abc583bb7af2a53c118a051aa3fd78849542a4e
scapotondo <sebastian.capotondo@gmail.com>
2017-04-23 19:29:13 -0300

    ordenesId

--- Persistencia/src/entity/OrdenDeProduccionCompletaEntity.java
+++ Persistencia/src/entity/OrdenDeProduccionCompletaEntity.java
@@ -1,10 +1,18 @@
 package entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="OrdenDeProduccionCompleta")
 public class OrdenDeProduccionCompletaEntity extends OrdenDeProduccionEntity{
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int ordenCompleta_id; 
+	
 	public OrdenDeProduccionCompletaEntity(){}
 }
