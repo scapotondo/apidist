fb6cb9d74ddfb1b72a5366839c23e97ed23a7280
scapotondo <sebastian.capotondo@gmail.com>
2017-04-23 15:33:03 -0300

    Ids

--- Persistencia/src/entity/ItemPrendaEntity.java
+++ Persistencia/src/entity/ItemPrendaEntity.java
@@ -5,6 +5,9 @@ import java.io.Serializable;
 import javax.persistence.Embeddable;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -12,6 +15,10 @@ import javax.persistence.Table;
 @Embeddable
 @Table(name="ItemPrenda")
 public class ItemPrendaEntity implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int nroPedido;
 	
 	@ManyToOne(targetEntity=PedidoPrendasEntity.class)
 	@Embedded
--- Persistencia/src/entity/OrdenDeProduccionEntity.java
+++ Persistencia/src/entity/OrdenDeProduccionEntity.java
@@ -5,6 +5,9 @@ import java.io.Serializable;
 import javax.persistence.Embeddable;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -13,6 +16,10 @@ import javax.persistence.Table;
 @Embeddable
 @Table(name="OrdenDeProduccion")
 public class OrdenDeProduccionEntity implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int nroPedido;
 	
 	@OneToOne
 	@Embedded
