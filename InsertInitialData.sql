fca1b305853b0382bf6c41600830589cbd8c66d5
emaf <ema@xamarin.com>
2017-05-25 21:59:52 -0300

    Adds initial SQL data

---
+++ InsertInitialData.sql
@@ -0,0 +1,13 @@
+Use ApiDist;
+
+INSERT INTO Sucursal 
+VALUES (1, "Avenida Siempre Viva 123", "Siempre Viva");
+
+INSERT INTO Sucursal 
+VALUES (2, "Calle Falsa 123", "Calle Falsa");
+
+INSERT INTO Sucursal 
+VALUES (3, "Rivendel 758", "Rivendel");
+
+INSERT INTO Sucursal 
+VALUES (4, "Tatooine 523", "Tatooine");
