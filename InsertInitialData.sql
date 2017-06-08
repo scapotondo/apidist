acb00a1a1ef5c91dee8e2f8dd5b99bd4f8531bed
Javier Capotondo <jcapotondo@gmail.com>
2017-06-08 19:59:12 -0300

    script prendas arregladas

--- InsertInitialData.sql
+++ InsertInitialData.sql
@@ -47,32 +47,99 @@ INSERT INTO MateriaPrima
 VALUES (5, 10, "Algodon peruano");
 
 
+
+-- PRENDA 1
 INSERT INTO Prenda
 VALUES (1, 100, "Remera cuello redondo", 0, "Remera", 10);
+
 
 INSERT INTO PrendaEntity_coloresValidos
 VALUES (1, 1); -- Remera Roja
 
+
+INSERT INTO PrendaEntity_tallesValidos
+VALUES (1, 's'); -- Talle s
+
+INSERT INTO PrendaEntity_tallesValidos
+VALUES (1, 'm'); -- Talle m
+
+INSERT INTO PrendaEntity_tallesValidos
+VALUES (1, 'l'); -- Talle l
+
+
+INSERT INTO Confeccion
+VALUES (1, 'Tela necesaria', 3.5, 1, 1); -- confeccion(id, detalle, tiempoProd, areaProd_codigo, prenda_id)
+
+
+INSERT INTO Insumo
+VALUES (1, 10, 0.5, 3, 2, 1); -- Insumo (id, cantidad, desperdicio, precio, codigo materia prima, confeccion_id)
+
+
+
+
+-- PRENDA 2
 INSERT INTO Prenda
 VALUES (2, 100, "Remera cuello v", 1, "Remera", 10);
+
 
 INSERT INTO PrendaEntity_coloresValidos
 VALUES (2, 2); -- Remera Negro
 
+
+INSERT INTO PrendaEntity_tallesValidos
+VALUES (2, 'xs'); -- Talle xs
+
+INSERT INTO PrendaEntity_tallesValidos
+VALUES (2, 's'); -- Talle s
+
+INSERT INTO PrendaEntity_tallesValidos
+VALUES (2, 'm'); -- Talle m
+
+INSERT INTO PrendaEntity_tallesValidos
+VALUES (2, 'l'); -- Talle l
+
+INSERT INTO PrendaEntity_tallesValidos
+VALUES (2, 'xl'); -- Talle xl
+
+
+INSERT INTO Confeccion
+VALUES (2, 'Algodon necesario', 5.5, 2, 2); -- confeccion(id, detalle, tiempoProd, areaProd_codigo, prenda_id)
+
+
+INSERT INTO Insumo
+VALUES (2, 5, 0, 5, 5, 2); -- Insumo (id, cantidad, desperdicio, precio, codigo materia prima, confeccion_id)
+
+
+
+-- PRENDA 3
 INSERT INTO Prenda
 VALUES (3, 100, "Pantalon jean", 1, "Pantalon", 10);
 
 INSERT INTO PrendaEntity_coloresValidos
 VALUES (3, 2); -- Pantalon Negro
 
+INSERT INTO PrendaEntity_tallesValidos
+VALUES (3, 's'); -- Talle s
+
+INSERT INTO PrendaEntity_tallesValidos
+VALUES (3, 'm'); -- Talle m
+
+
+INSERT INTO Confeccion
+VALUES (3, 'Botones necesarios', 1.5, 3, 3); -- confeccion(id, detalle, tiempoProd, areaProd_codigo, prenda_id)
+
+
+INSERT INTO Insumo
+VALUES (3, 5, 0, 5, 1, 3); -- Insumo (id, cantidad, desperdicio, precio, codigo materia prima, confeccion_id)
+
+
 
 INSERT INTO StockPrenda
-VALUES (1, 0, 1, 1.5, "", NOW(), "M", "AAA", null, 1, null);
+VALUES (3, 10, 1, 1.5, "", NOW(), "M", "AAA", null, 3, null); -- stock
 
 INSERT INTO StockPrenda
-VALUES (2, 0, 1, 1.5, "", NOW(), "M", "AAA", null, 2, null);
+VALUES (2, 0, 1, 1.5, "", NOW(), "M", "AAA", null, 2, null); -- stock
 
 INSERT INTO StockPrenda
-VALUES (3, 10, 1, 1.5, "", NOW(), "M", "AAA", null, 3, null);
-
+VALUES (1, 0, 1, 1.5, "", NOW(), "M", "AAA", null, 1, null); -- stock
 
